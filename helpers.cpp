f4af205b63762c38cf718f32878d20b5cc481bc8
Steve King <sk@metrokings.com>
2019-03-23 17:34:30 -0700

    Delete row from source table when moving.

--- helpers.cpp
+++ helpers.cpp
@@ -5,9 +5,8 @@ QList<QTableWidgetItem*> takeRow(QTableWidget* tbl, int row)
 {
     QList<QTableWidgetItem*> rowItems;
     for (int col = 0, colCount = tbl->columnCount(); col < colCount; ++col)
-    {
         rowItems << tbl->takeItem(row, col);
-    }
+    tbl->removeRow(row);
     return rowItems;
 }
 
@@ -16,11 +15,10 @@ void addRow(QTableWidget* tbl, const QList<QTableWidgetItem*>& rowItems)
 {
     if( rowItems.empty())
         return;
+
     int row = tbl->rowCount();
     tbl->insertRow(row);
 
     for (int col = 0, colCount = tbl->columnCount(); col < colCount; ++col)
-    {
         tbl->setItem(row, col, rowItems.at(col));
-    }
 }
